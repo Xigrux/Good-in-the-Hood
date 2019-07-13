891b8a75f02b17023ba93b6f7d82e1f0e78c4655
Xigrux <xing.jingyuan@outlook.com>
2019-07-13 13:11:25 -0400

    render poi on search

--- public/script/main.js
+++ public/script/main.js
@@ -8,128 +8,393 @@ $(document).ready(function() {
     zoom: 12 // starting zoom
   });
 
-  var geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken, // Set the access token
-    mapboxgl: mapboxgl, // Set the mapbox-gl instance
-    marker: false, // Do not use the default marker style
-    bbox: [-77.210763, 38.803367, -76.853675, 39.052643] // Set the bounding box coordinates
-  });
-
   map.addControl(
-    new MapboxGeocoder({
+    (geocoder = new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl
-    })
+    }))
   );
 
-  //   searchResult = {
-  //     type: "Feature",
-  //     properties: {
-  //       "@id": "node/207717952",
-  //       amenity: "kindergarten",
-  //       name: "CPE Pause-Parent-Enfant"
-  //     },
-  //     geometry: {
-  //       type: "Point",
-  //       coordinates: [-73.63809, 45.46879]
-  //     },
-  //     id: "node/207717952"
-  //   };
+  var searchResult;
 
-  $.getJSON("../data/json/kindergarten.json", function(json) {
-    var markers = json.features;
-    markers.forEach(function(marker) {
-      // console.log(marker);
-      // create a HTML element for each feature
-      var el = document.createElement("div");
-      el.className = "marker kindergarten";
-
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
-
-      //   //   Distance calculator
-      //   var options = { units: "kilometers" };
-      //   Object.defineProperty(marker.properties, "distance", {
-      //     value: turf.distance(searchResult, marker.geometry, options),
-      //     writable: true,
-      //     enumerable: true,
-      //     configurable: true
-      //   });
-      //   var roundedDistance = Math.round(marker.properties.distance * 10) / 10;
-
-      new mapboxgl.Marker(el)
-        .setLngLat(marker.geometry.coordinates)
-        .setPopup(
-          new mapboxgl.Popup({ offset: 25 }) // add popups
-            .setHTML(
-              "<h3>" +
-                marker.properties.name +
-                "</h3><p>" +
-                marker.properties.amenity +
-                "</p><p>"
-              // +
-              // roundedDistance +
-              // "</p>"
-            )
-        )
-        .addTo(map);
-
-      //   console.log(
-      //     marker.properties.distance + " " + marker.geometry.coordinates
-      //   );
-    });
+  geocoder.on("result", function(ev) {
+    searchResult = ev.result.geometry;
+    console.log(ev.result.geometry);
+    $(".marker").remove();
+    renderMarker();
   });
 
-  // CREATE METRO
-  $.getJSON("../data/json/metro.json", function(json) {
-    var markers = json.features;
-    markers.forEach(function(marker) {
-      // console.log(marker);
-      // create a HTML element for each feature
-      var el = document.createElement("div");
-      el.className = "marker metro";
+  function renderMarker() {
+    console.log("rendering markers");
+    $.getJSON("../data/json/kindergarten.json", function(json) {
+      //   ADD KINDERGATEN
+      var markers = json.features;
+      markers.forEach(function(marker) {
+        // console.log(marker);
+        // create a HTML element for each feature
+        var el = document.createElement("div");
+        el.className = "marker kindergarten";
 
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el).setLngLat([marker.xLong, marker.yLat]).addTo(map);
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .addTo(map);
 
-      //   //   Distance calculator
-      //   var options = { units: "kilometers" };
-      //   Object.defineProperty(marker, "distance", {
-      //     value: turf.distance(
-      //       searchResult,
-      //       [marker.xLong, marker.yLat],
-      //       options
-      //     ),
-      //     writable: true,
-      //     enumerable: true,
-      //     configurable: true
-      //   });
-      //   var roundedDistance = Math.round(marker.distance * 10) / 10;
+        if (searchResult) {
+          //   Distance calculator
+          var options = { units: "kilometers" };
+          Object.defineProperty(marker.properties, "distance", {
+            value: turf.distance(
+              searchResult.coordinates,
+              marker.geometry,
+              options
+            ),
+            writable: true,
+            enumerable: true,
+            configurable: true
+          });
+          var roundedDistance =
+            Math.round(marker.properties.distance * 10) / 10;
+        }
 
-      new mapboxgl.Marker(el)
-        .setLngLat([marker.xLong, marker.yLat])
-        .setPopup(
-          new mapboxgl.Popup({ offset: 25 }) // add popups
-            .setHTML(
-              "<h3>" + marker.MetroStation + "</h3><p>"
-              //  +
-              // roundedDistance +
-              // "</p>"
-            )
-        )
-        .addTo(map);
+        //   ADD POPUP
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                "<h3>" +
+                  marker.properties.name +
+                  "</h3><p>" +
+                  marker.properties.amenity +
+                  "</p><p>" +
+                  roundedDistance +
+                  "</p>"
+              )
+          )
+          .addTo(map);
 
-      //   console.log(
-      //     marker.properties.distance + " " + marker.geometry.coordinates
-      //   );
+        //   console.log(
+        //     marker.properties.distance + " " + marker.geometry.coordinates
+        //   );
+      });
     });
-  });
 
-  //   var searchResult;
-  //   geocoder.on("result", function(ev) {
-  //     searchResult = ev.result.geometry;
-  //     map.getSource("single-point").setData(searchResult);
-  //   });
+    // CREATE METRO
+    $.getJSON("../data/json/metro.json", function(json) {
+      //   ADD MARKER
+      var markers = json.features;
+      markers.forEach(function(marker) {
+        // console.log(marker);
+        // create a HTML element for each feature
+        var el = document.createElement("div");
+        el.className = "marker metro";
 
-  // new mapboxgl.Marker().setLngLat(searchResult.geometry.coordinates).addTo(map);
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+          .setLngLat([marker.xLong, marker.yLat])
+          .addTo(map);
+
+        if (searchResult) {
+          console.log("redering the distance");
+          //   Distance calculator
+          var options = { units: "kilometers" };
+          Object.defineProperty(marker, "distance", {
+            value: turf.distance(
+              searchResult.coordinates,
+              [marker.xLong, marker.yLat],
+              options
+            ),
+            writable: true,
+            enumerable: true,
+            configurable: true
+          });
+          var roundedDistance = Math.round(marker.distance * 10) / 10;
+        }
+
+        //   ADD POPUP
+        new mapboxgl.Marker(el)
+          .setLngLat([marker.xLong, marker.yLat])
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                "<h3>" +
+                  marker.MetroStation +
+                  "</h3><p>" +
+                  roundedDistance +
+                  "</p>"
+              )
+          )
+          .addTo(map);
+
+        // console.log(marker.distance + " " + [marker.xLong, marker.yLat]);
+      });
+    });
+
+    // CREATE CLINIC
+    $.getJSON("../data/json/clinic.json", function(json) {
+      //   ADD MARKER
+      var markers = json.features;
+      markers.forEach(function(marker) {
+        // console.log(marker);
+        // create a HTML element for each feature
+        var el = document.createElement("div");
+        el.className = "marker clinic";
+
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .addTo(map);
+
+        if (searchResult) {
+          //   Distance calculator
+          var options = { units: "kilometers" };
+          Object.defineProperty(marker.properties, "distance", {
+            value: turf.distance(
+              searchResult.coordinates,
+              marker.geometry,
+              options
+            ),
+            writable: true,
+            enumerable: true,
+            configurable: true
+          });
+          var roundedDistance =
+            Math.round(marker.properties.distance * 10) / 10;
+        }
+
+        //   ADD POPUP
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                "<h3>" +
+                  marker.properties.name +
+                  "</h3><p>" +
+                  marker.properties.amenity +
+                  "</p><p>" +
+                  roundedDistance +
+                  "</p>"
+              )
+          )
+          .addTo(map);
+
+        //   console.log(
+        //     marker.properties.distance + " " + marker.geometry.coordinates
+        //   );
+      });
+    });
+
+    // CREATE SCHOOL
+    $.getJSON("../data/json/schools.json", function(json) {
+      //   ADD MARKER
+      var markers = json;
+      markers.forEach(function(marker) {
+        // console.log(marker);
+        // create a HTML element for each feature
+        var el = document.createElement("div");
+        el.className = "marker school";
+
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+          .setLngLat([marker.xLong, marker.yLat])
+          .addTo(map);
+
+        if (searchResult) {
+          //   Distance calculator
+          var options = { units: "kilometers" };
+          Object.defineProperty(marker, "distance", {
+            value: turf.distance(
+              searchResult.coordinates,
+              [marker.xLong, marker.yLat],
+              options
+            ),
+            writable: true,
+            enumerable: true,
+            configurable: true
+          });
+          var roundedDistance = Math.round(marker.distance * 10) / 10;
+        }
+
+        //   ADD POPUP
+        new mapboxgl.Marker(el)
+          .setLngLat([marker.xLong, marker.yLat])
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                "<h3>" +
+                  marker.NOM_COURT_ORGNS +
+                  "</h3><p>" +
+                  roundedDistance +
+                  "</p>"
+              )
+          )
+          .addTo(map);
+
+        //   console.log(
+        //     marker.properties.distance + " " + marker.geometry.coordinates
+        //   );
+      });
+    });
+
+    // CREATE PARK
+    $.getJSON("../data/json/park.json", function(json) {
+      //   ADD MARKER
+      var markers = json.features;
+      markers.forEach(function(marker) {
+        // console.log(marker);
+        // create a HTML element for each feature
+        var el = document.createElement("div");
+        el.className = "marker park";
+
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .addTo(map);
+
+        if (searchResult) {
+          //   Distance calculator
+          var options = { units: "kilometers" };
+          Object.defineProperty(marker.properties, "distance", {
+            value: turf.distance(
+              searchResult.coordinates,
+              marker.geometry,
+              options
+            ),
+            writable: true,
+            enumerable: true,
+            configurable: true
+          });
+          var roundedDistance =
+            Math.round(marker.properties.distance * 10) / 10;
+        }
+
+        //   ADD POPUP
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                "<h3>" +
+                  marker.properties.leisure +
+                  "</h3><p>" +
+                  roundedDistance +
+                  "</p>"
+              )
+          )
+          .addTo(map);
+
+        //   console.log(
+        //     marker.properties.distance + " " + marker.geometry.coordinates
+        //   );
+      });
+    });
+
+    // CREATE GROGERIES
+    $.getJSON("../data/json/groceries.json", function(json) {
+      //   ADD MARKER
+      var markers = json.features;
+      markers.forEach(function(marker) {
+        // console.log(marker);
+        // create a HTML element for each feature
+        var el = document.createElement("div");
+        el.className = "marker grocery";
+
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .addTo(map);
+
+        if (searchResult) {
+          //   Distance calculator
+          var options = { units: "kilometers" };
+          Object.defineProperty(marker.properties, "distance", {
+            value: turf.distance(
+              searchResult.coordinates,
+              marker.geometry,
+              options
+            ),
+            writable: true,
+            enumerable: true,
+            configurable: true
+          });
+          var roundedDistance =
+            Math.round(marker.properties.distance * 10) / 10;
+        }
+
+        //   ADD POPUP
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                "<h3>" +
+                  marker.properties.name +
+                  "</h3><p>" +
+                  roundedDistance +
+                  "</p>"
+              )
+          )
+          .addTo(map);
+
+        //   console.log(
+        //     marker.properties.distance + " " + marker.geometry.coordinates
+        //   );
+      });
+    });
+
+    // CREATE GYM
+    $.getJSON("../data/json/gym.json", function(json) {
+      //   ADD MARKER
+      var markers = json.features;
+      markers.forEach(function(marker) {
+        // console.log(marker);
+        // create a HTML element for each feature
+        var el = document.createElement("div");
+        el.className = "marker gym";
+
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .addTo(map);
+
+        if (searchResult) {
+          //   Distance calculator
+          var options = { units: "kilometers" };
+          Object.defineProperty(marker.properties, "distance", {
+            value: turf.distance(
+              searchResult.coordinates,
+              marker.geometry,
+              options
+            ),
+            writable: true,
+            enumerable: true,
+            configurable: true
+          });
+          var roundedDistance =
+            Math.round(marker.properties.distance * 10) / 10;
+        }
+
+        //   ADD POPUP
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                "<h3>" +
+                  marker.properties.name +
+                  "</h3><p>" +
+                  roundedDistance +
+                  "</p>"
+              )
+          )
+          .addTo(map);
+
+        //   console.log(
+        //     marker.properties.distance + " " + marker.geometry.coordinates
+        //   );
+      });
+    });
+  }
 });
