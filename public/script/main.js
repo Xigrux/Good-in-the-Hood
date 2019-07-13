8f2dacfe20ba80959485b9d8e7e7838dcadd4ea3
Xigrux <xing.jingyuan@outlook.com>
2019-07-13 13:47:08 -0400

    styling

--- public/script/main.js
+++ public/script/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   var map = new mapboxgl.Map({
     container: "map", // container id
     style: "mapbox://styles/mapbox/light-v10", // stylesheet location
-    center: [-73.567256, 45.501689], // starting position
+    center: [-73.607256, 45.500089], // starting position
     zoom: 12 // starting zoom
   });
 
